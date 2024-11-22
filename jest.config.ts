8072f7d5ccc31c7d8373b11145c504b6eb928658
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-11-22 17:09:03 -0300

    refact: configurando jest

--- jest.config.ts
+++ jest.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
    // clearMocks: false,
 
    // Indicates whether the coverage information should be collected while executing the test
-   collectCoverage: true,
+   collectCoverage: false,
 
    // An array of glob patterns indicating a set of files for which coverage information should be collected
    collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts"],
@@ -175,7 +175,7 @@ const config: Config = {
    // unmockedModulePathPatterns: undefined,
 
    // Indicates whether each individual test should be reported during the run
-   // verbose: undefined,
+   verbose: true,
 
    // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
    // watchPathIgnorePatterns: [],
